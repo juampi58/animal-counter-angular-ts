aa6fa064dbab68a3b4af7b014040e60f8dacb7f9
juampi58 <juanpabloloza@gmail.com>
2025-05-02 17:02:13 +0300

    shows informative messages

--- src/animal-form.component.ts
+++ src/animal-form.component.ts
@@ -1,17 +1,18 @@
 import { Component } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 import { AnimalService } from "./animal.service";
 
 @Component({
     selector: 'app-animal-form',
     standalone: true,
-    imports: [FormsModule],
+    imports: [FormsModule, CommonModule],
     templateUrl: './animal-form.component.html'
 })
 
 export class AnimalFormComponent{
     animalName=''
-
+    showMessage= false
     constructor(private animalService: AnimalService){}
 
     addAnimal(event:Event){
@@ -19,6 +20,8 @@ export class AnimalFormComponent{
         if(this.animalName.trim()){
             this.animalService.addAnimal(this.animalName.trim())
             this.animalName = ''
+            this.showMessage = true
+            setTimeout(()=>this.showMessage=false, 2000)
         }
     }
 }

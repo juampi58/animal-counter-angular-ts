c85b6f9d4977afd652cb4a637bc6b316a6447acf
juampi58 <juanpabloloza@gmail.com>
2025-05-03 13:31:27 +0300

    adds filter through routing

--- src/animal-list.component.ts
+++ src/animal-list.component.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { AnimalService } from "./animal.service";
+import { ActivatedRoute, RouterLink } from "@angular/router";
 @Component({
     selector:'app-animal-list',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, RouterLink],
     templateUrl: './animal-list.component.html'
 })
 
 export class AnimalListComponent implements OnInit, OnDestroy{
     private intervalId: any
     private seconds: number = 0
-    constructor(private animalService: AnimalService){}
+    filter : string | null = null
+    constructor(
+        private animalService: AnimalService,
+        private route: ActivatedRoute
+    ){}
     ngOnInit(): void {
+        this.route.paramMap.subscribe(params=>{
+            const type= params.get('type')
+            this.filter = type
+        })
         this.animalService.loadAnimalsFromFakeApi()
 
         this.intervalId = setInterval(()=>{
@@ -25,7 +34,8 @@ export class AnimalListComponent implements OnInit, OnDestroy{
         console.log(`AnimlasListComponent  destroyed. Timer stopped`)
     }
     get animals(){
-        return this.animalService.getAnimals()
+        const all = this.animalService.getAnimals()
+        return this.filter? all.filter(a=>a.name === this.filter): all
     }
     clear(){
         this.animalService.clearAnimals()
--- src/app.routes.ts
+++ src/app.routes.ts
@@ -15,6 +15,10 @@ export const routes:Routes = [
             {
                 path: '',
                 component: AnimalListComponent
+            },
+            {
+                path: ':type',
+                component: AnimalListComponent
             }
         ]
     }
